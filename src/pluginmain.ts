51b482f0dc7edd755d98a6f0d74fdaa2ae50e5be
Mike Stall <mikestall@hotmail.com>
2016-08-17 00:07:43 -0700

    Initial add for Turnout Report.

--- src/pluginmain.ts
+++ src/pluginmain.ts
@@ -4,101 +4,227 @@
 
 import * as trc from '../node_modules/trclib/trc2';
 import * as html from '../node_modules/trclib/trchtml';
-import * as gps from '../node_modules/trclib/gps';
-import * as trcFx from '../node_modules/trclib/trcfx'; 
+import * as trcFx from '../node_modules/trclib/trcfx';
 
 declare var $: any; // external definition for JQuery 
 
+class Helpers {
+    private static isTrue(val: string): boolean {
+        if ((val == "1") || (val.toLowerCase() == "yes")) {
+            return true;
+        }
+        return false;
+    }
+
+    // return a string of (a/b) as a percent. 
+    public static Per(a: number, b: number): string {
+        if (b == 0) {
+            return "--";
+        }
+        return (a * 100 / b).toFixed(1) + "%";
+    }
+
+    public static getSupportersThatNotVoted(
+        data: trc.ISheetContents): trc.ISheetContents {
+        var cVoted: string[] = data["XVoted"];
+        var cSupporter: string[] = data["Supporter"];
+
+        var x = trc.SheetContents.KeepRows(
+            data, (iRow) =>
+                Helpers.isTrue(cSupporter[iRow]) &&
+                !Helpers.isTrue(cVoted[iRow])
+        );
+        return x;
+    }
+
+    public static getTotal(
+        data : trc.ISheetContents) : trc.ISheetContents
+    {
+        var cVoted: string[] = data["XVoted"];
+        
+        var grandTotal = 0;
+        var grandTotalVoted = 0;
+
+
+        // Count them
+        for (var iRow = 0; iRow < cVoted.length; iRow++) {
+            grandTotal++;            
+            if (Helpers.isTrue(cVoted[iRow])) {                
+                grandTotalVoted++;
+            }            
+        }     
+
+        var x : trc.ISheetContents = {};
+        x["Total"] = [grandTotal.toString()];
+        x["Voted"] = [grandTotalVoted.toString()];
+        x["Turnout%"] = [Helpers.Per(grandTotalVoted, grandTotal)];
+        return x;       
+    }
+
+    // Make a histogram pivoted on columnName, with counts for "Total" and "Voted"
+    // data - raw sheet
+    // columnName - column to pivot on. 
+    // labels - optional mapping to provide friendly names
+    public static makeHist(
+        data: trc.ISheetContents,
+        columnName: string,
+        labels: { [key: string]: string; }): trc.ISheetContents {
+
+        var c: string[] = data[columnName];
+
+        var cVoted: string[] = data["XVoted"];
+
+        var d: any = {};
+
+        var grandTotalVoted = 0;
+        // Count them
+        for (var iRow = 0; iRow < c.length; iRow++) {
+            var key: string = c[iRow];
+
+            var entry = d[key];
+            if (entry == undefined) {
+                entry = [0, 0];
+            }
+            entry[0]++;
+            if (Helpers.isTrue(cVoted[iRow])) {
+                entry[1]++;
+                grandTotalVoted++;
+            }
+            d[key] = entry;
+        }
+
+        // Convert to a sheet 
+
+        var result: trc.ISheetContents = {};
+        var cValue: string[] = [];
+        var cTotal: string[] = [];
+        var cVoted: string[] = [];
+        var cPercentVoted: string[] = [];
+        var cPercentOfTotal: string[] = [];
+        var cPercentOfVoted: string[] = [];
+
+        result[columnName] = cValue;
+        result["Total"] = cTotal;
+        result["Voted"] = cVoted;
+        result["Turnout%"] = cPercentVoted;
+        result["PercentOfTotal"] = cPercentOfTotal;
+        result["PercentOfVoted"] = cPercentOfVoted;
+
+        for (var key in d) {
+            var entry = d[key];
+            var label = key;
+            if (key == "0") {
+                key = "";
+            }
+
+            if (labels != null) {
+                var x = labels[key];
+                if (x != undefined) {
+                    label = x;
+                }
+            }
+
+            cValue.push(label);
+
+            var total = entry[0];
+            var voted = entry[1];
+            cTotal.push(total);
+            cVoted.push(voted);
+
+            var p = Helpers.Per(voted, total);
+            cPercentVoted.push(p);
+
+            var p = Helpers.Per(total, c.length);
+            cPercentOfTotal.push(p);
+
+            var p = Helpers.Per(voted, grandTotalVoted);
+            cPercentOfVoted.push(p);
+        }
+
+        return result;
+    }
+}
+
 export class MyPlugin {
     private _sheet: trc.Sheet;
-    private _gps : gps.IGpsTracker;
-    private _options : trc.PluginOptionsHelper;
+    private _options: trc.PluginOptionsHelper;
 
     // Entry point called from brower. 
     // This creates real browser objects and passes in. 
     public static BrowserEntry(
-        sheet: trc.ISheetReference, 
-        opts : trc.IPluginOptions, 
-        next : (plugin : MyPlugin) => void 
-    ) : void {
-        var trcSheet = new trc.Sheet(sheet);                
+        sheet: trc.ISheetReference,
+        opts: trc.IPluginOptions,
+        next: (plugin: MyPlugin) => void
+    ): void {
+        var trcSheet = new trc.Sheet(sheet);
         var opts2 = trc.PluginOptionsHelper.New(opts, trcSheet);
 
-        // Track GPS location of device
-        var gpsTracker = new gps.GpsTracker(); // Only works in browser
-        gpsTracker.start(null); // ignore callback
 
         // Do any IO here...
-        
-        var plugin = new MyPlugin(trcSheet, opts2, gpsTracker);
+
+        var plugin = new MyPlugin(trcSheet, opts2);
         next(plugin);
     }
 
     // Expose constructor directly for tests. They can pass in mock versions. 
     public constructor(
         sheet: trc.Sheet,
-        opts2 : trc.PluginOptionsHelper, 
-        gpsTracker : gps.IGpsTracker
+        opts2: trc.PluginOptionsHelper
     ) {
-        this.resetUi();
-
         this._sheet = sheet; // Save for when we do Post
-        this._options = opts2;               
-        this._gps = gpsTracker;
+        this._options = opts2;
 
-        this.refresh();
+        this.refresh2();
     }
 
-    private refresh() {
-        this._sheet.getInfo(
-            (info) => {
-                this.updateInfo(info);
+
+    private refresh2() {
+        this._sheet.getSheetContents(
+            data => {
+                this.makeCharts(data);
+            }
+        );
+    }
+
+
+    private makeCharts(data: trc.ISheetContents): void {
+
+        var c1 = Helpers.getTotal(data);
+        var r = new html.RenderSheet("xtotal", c1);
+        r.render();       
+
+
+        // "Supporter"
+        var c1 = Helpers.makeHist(data, "Supporter", null);
+        var r = new html.RenderSheet("xsupporter", c1);
+        r.render();
+
+        var c1 = Helpers.makeHist(data, "Party", {
+            "1": "(1) Hard GOP",
+            "2": "(2) Soft GOP",
+            "3": "(3) Independent",
+            "4": "(4) Soft Dem",
+            "5": "(5) Hard Dem",
+            "": "Unidentified"
+        });
+        var r = new html.RenderSheet("xparty", c1);
+        r.render();
+
+        var c1 = Helpers.makeHist(data, "XTargetPri",
+            {
+                "1": "Targeted voter",
+                "": "Non targeted voter"
             });
-    }
+        var r = new html.RenderSheet("xtarget", c1);
+        r.render();
 
-    protected resetUi(): void {
-        // clear previous results
-        $('#main').empty();
-    }
+        var c1 = Helpers.makeHist(data, "ResultOfContact", null);
+        var r = new html.RenderSheet("xcontact", c1);
+        r.render();
 
-    // Display sheet info on HTML page
-    public updateInfo(info: trc.ISheetInfoResult): void {
-        $("#SheetName").text(info.Name);
-        $("#ParentSheetName").text(info.ParentName);
-        $("#SheetVer").text(info.LatestVersion);
-        $("#RowCount").text(info.CountRecords);
-
-        $("#LastRefreshed").text(new Date().toLocaleString());
-    }
-
-    // Example of a helper function.
-    public doubleit(val: number): number {
-        return val * 2;
-    }
-
-    // Demonstrate receiving UI handlers 
-    public onClickRefresh(): void {
-        this.refresh();
-    }
-
-    // SheetControl will render HTML controls that need to callback to the sheet control.
-    // The js expression in html is "_plugin._control", which must be passed to the renderer.
-    public _control : html.SheetControl; // Expose so HTML handlers can invoke. 
-
-    // downloading all contents and rendering them to HTML can take some time. 
-    public onGetSheetContents(): void {
-        html.Loading("contents");
-        //$("#contents").empty();
-        //$("#contents").text("Loading...");
-
-        trcFx.SheetEx.InitAsync(this._sheet, this._gps, (sheetEx)=>
-        {
-            this._sheet.getSheetContents((contents) => {
-                var render = new html.SheetControl("contents", sheetEx);
-                // could set other options on render() here
-                render.render();
-                this._control = render;
-            });
-        });        
+        var c1 = Helpers.getSupportersThatNotVoted(data);
+        var r = new html.RenderSheet("xs2", c1);
+        r.setColumns(["RecId", "FirstName", "LastName", "PrecinctName"]);
+        r.render();
     }
 }
