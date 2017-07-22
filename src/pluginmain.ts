be529aed9e56618b05f05fcb18fe63e57f4bb338
Voter-Science <Info@Voter-Science.com>
2017-07-22 15:51:26 -0700

    Be resilient if there's no XTargetPri column

--- src/pluginmain.ts
+++ src/pluginmain.ts
@@ -7,8 +7,8 @@ import * as html from 'trclib/trchtml';
 import * as trcFx from 'trclib/trcfx';
 import * as h from './helpers';
 declare var $: any; // external definition for JQuery 
-declare var google:any;
-declare var HeatmapOverlay:any;
+declare var google: any;
+declare var HeatmapOverlay: any;
 
 export class MyPlugin {
     private _sheet: trc.Sheet;
@@ -47,19 +47,18 @@ export class MyPlugin {
         this.refresh2(next);
     }
 
-    private refresh2(  next: (plugin: MyPlugin) => void) {
+    private refresh2(next: (plugin: MyPlugin) => void) {
         // $$$ cache this? 
-        this._sheet.findVersion(this._electionDate, 
-            (version : number ) => 
-            {
-                $("#date").html("This report is for <b>" + this._electionDate.toLocaleDateString() + "</b> and using canvassing results as of that time (version " + version + ")." );
-                var version : number;
+        this._sheet.findVersion(this._electionDate,
+            (version: number) => {
+                $("#date").html("This report is for <b>" + this._electionDate.toLocaleDateString() + "</b> and using canvassing results as of that time (version " + version + ").");
+                var version: number;
                 this._sheet.getSheetContents(
-                    data => {                        
+                    data => {
                         this.makeCharts(data);
 
                         next(this);
-                    }, undefined, undefined, undefined, version 
+                    }, undefined, undefined, undefined, version
                 );
             }, undefined);
     }
@@ -69,13 +68,13 @@ export class MyPlugin {
 
         var c1 = h.Helpers.getTotal(data);
         var r = new html.RenderSheet("xtotal", c1);
-        r.render();      
+        r.render();
 
         // "Supporter"
         var xsupporter = h.Helpers.makeHist(data, "Supporter", null);
         var r = new html.RenderSheet("xsupporter", xsupporter);
         r.render();
-        html.DownloadHelper.appendDownloadCsvButton(document.getElementById("xsupporter-download"), () => {return xsupporter;});
+        html.DownloadHelper.appendDownloadCsvButton(document.getElementById("xsupporter-download"), () => { return xsupporter; });
 
         var xparty = h.Helpers.makeHist(data, "Party", {
             "1": "(1) Hard GOP",
@@ -87,34 +86,40 @@ export class MyPlugin {
         });
         var r = new html.RenderSheet("xparty", xparty);
         r.render();
-        html.DownloadHelper.appendDownloadCsvButton(document.getElementById("xparty-download"), () => {return xparty;});
+        html.DownloadHelper.appendDownloadCsvButton(document.getElementById("xparty-download"), () => { return xparty; });
 
-        var xtarget = h.Helpers.makeHist(data, "XTargetPri",
-            {
-                "1": "Targeted voter",
-                "": "Non targeted voter"
-            });
-        var r = new html.RenderSheet("xtarget", xtarget);
-        r.render();
-        html.DownloadHelper.appendDownloadCsvButton(document.getElementById("xtarget-download"), () => {return xtarget;});
+        
+        if (data["XTargetPri"] != undefined)
+        {            
+            var xtarget = h.Helpers.makeHist(data, "XTargetPri",
+                {
+                    "1": "Targeted voter",
+                    "": "Non targeted voter"
+                });
+            var r = new html.RenderSheet("xtarget", xtarget);
+            r.render();
+            html.DownloadHelper.appendDownloadCsvButton(document.getElementById("xtarget-download"), () => { return xtarget; });
+        } else {
+            $('#xtarget').append($('<p>[No targets set]</p>'));
+        }
 
         var xcontact = h.Helpers.makeHist(data, "ResultOfContact", null);
         var r = new html.RenderSheet("xcontact", xcontact);
         r.render();
-        html.DownloadHelper.appendDownloadCsvButton(document.getElementById("xcontact-download"), () => {return xcontact;});
+        html.DownloadHelper.appendDownloadCsvButton(document.getElementById("xcontact-download"), () => { return xcontact; });
 
         var xs2 = h.Helpers.getSupportersThatNotVoted(data);
         var r = new html.RenderSheet("xs2", xs2);
         r.setColumns(["RecId", "FirstName", "LastName", "PrecinctName"]);
         r.render();
-        html.DownloadHelper.appendDownloadCsvButton(document.getElementById("xs2-download"), () => {return xs2;});
+        html.DownloadHelper.appendDownloadCsvButton(document.getElementById("xs2-download"), () => { return xs2; });
 
         // "precinct"
         var xprecinct = h.Helpers.makeHist(data, "PrecinctName", null);
         var r = new html.RenderSheet("xprecinct", xprecinct);
         r.render();
-        html.DownloadHelper.appendDownloadCsvButton(document.getElementById("xprecinct-download"), () => {return xprecinct;});
-        
+        html.DownloadHelper.appendDownloadCsvButton(document.getElementById("xprecinct-download"), () => { return xprecinct; });
+
         this.applyBootstrapStyling();
 
         var party1 = this.getHeatmapData(data, "Party", "1");
@@ -127,16 +132,16 @@ export class MyPlugin {
         this.createHeatmap(xvoted1, "xvoted-1-heatmap");
     }
 
-    private getHeatmapData(source:trc.ISheetContents, filterColumnName?:string, filterColumnValue?:any):any {
+    private getHeatmapData(source: trc.ISheetContents, filterColumnName?: string, filterColumnValue?: any): any {
         let count = source["Lat"].length;
 
         console.log(source);
 
-        var dic:any = {};
+        var dic: any = {};
 
-        var max:number = 0;
+        var max: number = 0;
 
-        for(let i:number=0; i<count; i++) {
+        for (let i: number = 0; i < count; i++) {
 
             if (filterColumnName) {
                 if (source[filterColumnName][i] != filterColumnValue) continue;
@@ -144,16 +149,16 @@ export class MyPlugin {
 
             let lat = source["Lat"][i];
             let lng = source["Long"][i];
-            
+
             let key = "".concat(lat, lng);
             if (!key) continue;
-            
+
             if (!dic[key]) {
-                dic[key] = { lat: lat, lng: lng, count: 1}
+                dic[key] = { lat: lat, lng: lng, count: 1 }
             }
             else {
                 dic[key]["count"]++;
-                if (dic[key]["count"] > max)  { max = dic[key]["count"];}
+                if (dic[key]["count"] > max) { max = dic[key]["count"]; }
             }
         }
 
@@ -164,46 +169,46 @@ export class MyPlugin {
             dataArray.push(dic[key]);
         });
 
-        return {max:max, data:dataArray};
+        return { max: max, data: dataArray };
     }
 
-    private createHeatmap(data:any, containerId:string): void {
+    private createHeatmap(data: any, containerId: string): void {
         // don't forget to add gmaps-heatmap.js
         //var myLatlng = new google.maps.LatLng(25.6586, -80.3568);
         var myLatlng = new google.maps.LatLng(data.data[0].lat, data.data[0].lng);
         // map options,
         var myOptions = {
-        zoom: 14,
-        center: myLatlng
+            zoom: 14,
+            center: myLatlng
         };
         // standard map
         var map = new google.maps.Map(document.getElementById(containerId), myOptions);
         // heatmap layer
-        var heatmap = new HeatmapOverlay(map, 
-        {
-            // radius should be small ONLY if scaleRadius is true (or small radius is intended)
-            "radius": 10,
-            "maxOpacity": .5, 
-            // scales the radius based on map zoom
-            "scaleRadius": false, 
-            // if set to false the heatmap uses the global maximum for colorization
-            // if activated: uses the data maximum within the current map boundaries 
-            //   (there will always be a red spot with useLocalExtremas true)
-            "useLocalExtrema": true,
-            // which field name in your data represents the latitude - default "lat"
-            latField: 'lat',
-            // which field name in your data represents the longitude - default "lng"
-            lngField: 'lng',
-            // which field name in your data represents the data value - default "value"
-            valueField: 'count'
-        }
+        var heatmap = new HeatmapOverlay(map,
+            {
+                // radius should be small ONLY if scaleRadius is true (or small radius is intended)
+                "radius": 10,
+                "maxOpacity": .5,
+                // scales the radius based on map zoom
+                "scaleRadius": false,
+                // if set to false the heatmap uses the global maximum for colorization
+                // if activated: uses the data maximum within the current map boundaries 
+                //   (there will always be a red spot with useLocalExtremas true)
+                "useLocalExtrema": true,
+                // which field name in your data represents the latitude - default "lat"
+                latField: 'lat',
+                // which field name in your data represents the longitude - default "lng"
+                lngField: 'lng',
+                // which field name in your data represents the data value - default "value"
+                valueField: 'count'
+            }
         );
 
         heatmap.setData(data);
     }
 
     //TODO: remove if/when html helper renders elements with Bootstrap styles
-    private applyBootstrapStyling():void {
+    private applyBootstrapStyling(): void {
         $("table").addClass("table table-striped");
         $("table").removeAttr("border");
 
